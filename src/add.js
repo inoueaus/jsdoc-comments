df72931b86be7d61b8206af490b691eafce7c307
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-05-08 16:14:39 +0900

    initial commit

---
+++ src/add.js
@@ -0,0 +1,10 @@
+/**
+ * A function that adds two numbers.
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ * https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html
+ */
+export const add = (num1, num2) => {
+  return num1 + num2;
+};
